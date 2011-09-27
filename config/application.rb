5dc5837bf979d05093ee36aa4f07deb5aeac7bdc
David Heath <david@davidheath.org>
2011-09-27 09:55:53 +0000

    Don't need activerecord.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Don't include all of rails, we don't need activerecord
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
