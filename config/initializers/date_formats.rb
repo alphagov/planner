a8d208668699aa2fd8ceb73d783783f46d295475
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-06-14 13:51:13 +0100

    Various copy changes.

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,4 +1,4 @@
 Date::DATE_FORMATS[:short_ordinal] = "%e %B %Y"
 Time::DATE_FORMATS[:short_ordinal] = "%e %B %Y"
-Date::DATE_FORMATS[:short_ordinal_day] = "%A, %e %B %Y"
-Time::DATE_FORMATS[:short_ordinal_day] = "%A, %e %B %Y"
+Date::DATE_FORMATS[:short_ordinal_day] = "%A %e %B %Y"
+Time::DATE_FORMATS[:short_ordinal_day] = "%A %e %B %Y"
