212b1b9b5835aa057f5bf24a8611f76689cc2874
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-07-02 11:30:23 +0100

    Fix test description to match its behaviour.

--- test/integration/bad_input_handling_test.rb
+++ test/integration/bad_input_handling_test.rb
@@ -1,7 +1,7 @@
 require 'integration_test_helper'
 
 class BadInputHandlingTest < ActionDispatch::IntegrationTest
-  test "should quickly return 400 if bad encodings in user input" do
+  test "should quickly return 200 if bad encodings in user input" do
     get "/maternity?wsdl=/..%c0%af../..%c0%af../..%c0%af../..%c0%af../etc/passwd"
     assert_equal 200, response.code.to_i
     assert ! response.body.match(/passwd/)
