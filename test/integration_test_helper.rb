07a551a8bce4f2baeb3b201cd841e192a066393b
James Stewart <jys@ketlai.co.uk>
2012-07-02 11:28:10 +0100

    Stop integration tests requiring slimmer/static
    Patch ActionController::Base so that the 'skip-slimmer' header is always set. This means the integration tests can run in isolation without requiring static/panopticon to be running.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -5,6 +5,13 @@ class ActionDispatch::IntegrationTest
   include Capybara::DSL
 end
 
+# Tell all requests to skip slimmer
+class ActionController::Base
+  before_filter proc {
+    response.headers[Slimmer::SKIP_HEADER] = "true"
+  }
+end
+
 Capybara.default_driver = :webkit
 Capybara.app = Rack::Builder.new do
   map "/" do
