5c1f5766e258f573e644b059eab0568a61c6e8e4
Gareth Rushgrove <gareth@morethanseven.net>
2012-08-26 18:52:43 +0100

    the path in graphite is suboptimal for multiple applications per hostname. Here's a possible solution

--- config/initializers/process_utilisation.rb
+++ config/initializers/process_utilisation.rb
@@ -2,5 +2,10 @@ require 'statsd'
 require 'rack-statsd'
 
 METRICS = Statsd.new('localhost', 8125)
-Rails.application.middleware.insert_before Rack::Lock, RackStatsD::ProcessUtilization, nil, nil, {:stats => METRICS} unless Rails.env.developent? or Rails.env.test?
+
+metric_path = []
+metric_path << `hostname -s`.chomp
+metric_path << 'planner'
+
+Rails.application.middleware.insert_before Rack::Lock, RackStatsD::ProcessUtilization, nil, nil, {:stats => METRICS, :hostname => metric_path.join('.')} unless Rails.env.developent? or Rails.env.test?
 
