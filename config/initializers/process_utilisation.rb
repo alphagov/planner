e22988312a130cd208ce78304461ae1a78c45a86
Gareth Rushgrove <gareth@morethanseven.net>
2012-08-26 18:31:18 +0100

    static isn't enabled for this app so that middleware isn't going to be present

--- config/initializers/process_utilisation.rb
+++ config/initializers/process_utilisation.rb
@@ -2,5 +2,5 @@ require 'statsd'
 require 'rack-statsd'
 
 METRICS = Statsd.new('localhost', 8125)
-Rails.application.middleware.insert_before ActionDispatch::Static, RackStatsD::ProcessUtilization, nil, nil, {:stats => METRICS} unless Rails.env.developent? or Rails.env.test?
+Rails.application.middleware.insert_before Rack::Lock, RackStatsD::ProcessUtilization, nil, nil, {:stats => METRICS} unless Rails.env.developent? or Rails.env.test?
 
